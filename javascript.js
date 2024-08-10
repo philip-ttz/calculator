fc9107b6874e682b16cb4bbb39c75809b9ee6cf1
philip <bigtea@Xubuntu.myguest.virtualbox.org>
2024-08-10 17:51:10 +0200

    adding necessary dom selectors

--- javascript.js
+++ javascript.js
@@ -1,3 +1,24 @@
+const number0=document.querySelector("#number0");
+const number1=document.querySelector("#number1");
+const number2=document.querySelector("#number2");
+const number3=document.querySelector("#number3");
+const number4=document.querySelector("#number4");
+const number5=document.querySelector("#number5");
+const number6=document.querySelector("#number6");
+const number7=document.querySelector("#number7");
+const number8=document.querySelector("#number8");
+const number9=document.querySelector("#number9");
+const ac=document.querySelector("#ac");
+const plusminus=document.querySelector("#plusminus");
+const percent=document.querySelector("#percent");
+const division=document.querySelector("#division");
+const multiplication=document.querySelector("#multiplication");
+const subtraction=document.querySelector("#subtraction");
+const addition=document.querySelector("#addition");
+const comma=document.querySelector("#comma");
+const equals=document.querySelector("#equals");
+const output=document.querySelector(".output");
+
 function add(a,b){
     return a+b;
 }
@@ -14,10 +35,6 @@ function divide(a,b){
     return a/b;
 }
 
-let number1=0;
-let operator='+';
-let number2=0;
-
 function operate(num1, op, num2){
     let result = 0;
     if(op==='+'){
@@ -31,3 +48,9 @@ function operate(num1, op, num2){
     }
     return result;
 }
+
+
+
+let firstnumber=0;
+let operator='+';
+let secondnumber=0;
