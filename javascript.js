ad06ea55b88c731042581a2fd9d22ef831368f57
philip <bigtea@Xubuntu.myguest.virtualbox.org>
2024-08-12 21:38:41 +0200

    making activebtn work

--- javascript.js
+++ javascript.js
@@ -1,22 +1,22 @@
-const number0=document.querySelector("#number0");
-const number1=document.querySelector("#number1");
-const number2=document.querySelector("#number2");
-const number3=document.querySelector("#number3");
-const number4=document.querySelector("#number4");
-const number5=document.querySelector("#number5");
-const number6=document.querySelector("#number6");
-const number7=document.querySelector("#number7");
-const number8=document.querySelector("#number8");
-const number9=document.querySelector("#number9");
-const ac=document.querySelector("#ac");
-const plusminus=document.querySelector("#plusminus");
-const percent=document.querySelector("#percent");
-const division=document.querySelector("#division");
-const multiplication=document.querySelector("#multiplication");
-const subtraction=document.querySelector("#subtraction");
-const addition=document.querySelector("#addition");
-const comma=document.querySelector("#comma");
-const equals=document.querySelector("#equals");
+const number0=document.querySelector(".number0");
+const number1=document.querySelector(".number1");
+const number2=document.querySelector(".number2");
+const number3=document.querySelector(".number3");
+const number4=document.querySelector(".number4");
+const number5=document.querySelector(".number5");
+const number6=document.querySelector(".number6");
+const number7=document.querySelector(".number7");
+const number8=document.querySelector(".number8");
+const number9=document.querySelector(".number9");
+const ac=document.querySelector(".ac");
+const plusminus=document.querySelector(".plusminus");
+const percent=document.querySelector(".percent");
+const division=document.querySelector(".division");
+const multiplication=document.querySelector(".multiplication");
+const subtraction=document.querySelector(".subtraction");
+const addition=document.querySelector(".addition");
+const comma=document.querySelector(".comma");
+const equals=document.querySelector(".equals");
 const output=document.querySelector(".output p");
 
 function add(a,b){
@@ -167,4 +167,8 @@ ac.addEventListener('click', ()=>{
     updateDisplay(0, true);
     displayvalue=parseInt(output.textContent,10);
     currentState=1;
+    division.classList.remove("activebtn");
+    multiplication.classList.remove("activebtn");
+    subtraction.classList.remove("activebtn");
+    addition.classList.remove("activebtn");
 })
